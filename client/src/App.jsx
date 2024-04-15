2b94c6b6e72f9f9f9c207c5367d46274f7f12b5d
MIkeyJ <currygeigei@gmail.com>
2024-04-15 11:09:18 +0800

    feat(client/src/App.jsx): 建置分頁及路由

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
-  return <h1 className="text-3xl text-red-500">Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
