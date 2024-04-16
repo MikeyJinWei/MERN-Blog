e23fad5cc0bf1ccc410fda90850d3b61ba2d7b0a
MIkeyJ <currygeigei@gmail.com>
2024-04-16 23:25:06 +0800

    feat(api/index.js): 連接至 DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT;
 const app = express();
-const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
